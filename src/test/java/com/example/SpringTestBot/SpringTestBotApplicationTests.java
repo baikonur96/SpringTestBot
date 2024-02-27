30ea5d91e896212a1b675a187c575f6147bf639d
Vladimir Pozharski <Baikonur96@yandex.ru>
2024-02-27 23:55:14 +0300

    init

---
+++ src/test/java/com/example/SpringTestBot/SpringTestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringTestBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
